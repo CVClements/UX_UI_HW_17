a122d959f74b3ea98de92934a02fc2802dc09e11
CVClements <98189559+CVClements@users.noreply.github.com>
2022-08-01 18:27:54 -0600

    8-1

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,5 @@
 $(".button").hover(function() {
-   console.log('hovering');
    $(this).css("background-color", "#FF6F65")
 }, function() {
-   console.log('leaving hover');
    $(this).css("background-color", "")
 });
